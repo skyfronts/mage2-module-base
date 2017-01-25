795d83933781bc8f466d8c0a9b02c12f46ed2c32
Hau Danh <hau@yasova.co>
2017-01-25 14:01:52 +0700

    First commit

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © 2016 Skyfronts. All rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Skyfronts_Base to newer
+ * versions in the future.
+ *
+ * @category    Skyfronts
+ * @package     Skyfronts_Base
+ * @author      Skyfronts team
+ * @copyright   Copyright (c) Skyfronts (https://www.skyfronts.com)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Skyfronts_Base',
+    __DIR__
+);
